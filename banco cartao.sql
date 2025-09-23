484a78b144802c8edb74ca0e290b929780adef87
alu180 <162990810+alu180@users.noreply.github.com>
2025-09-23 13:18:44 -0300

    Add files via upload
    banco sql tabelas completas faltando testes e com uma parcialmente populada faltando as consultas

---
+++ banco cartao.sql
@@ -0,0 +1,88 @@
+-- SCHEMA: flexcard
+
+-- DROP SCHEMA IF EXISTS flexcard ;
+
+CREATE SCHEMA IF NOT EXISTS flexcard
+    AUTHORIZATION postgres;
+
+	create table cartao_aluno (
+	nome VARCHAR (100) NOT NULL,
+	matricula VARCHAR (10) PRIMARY KEY,
+	cpf VARCHAR (11)NOT NULL UNIQUE,
+	data_emissao DATE NOT NULL,
+	data_validade DATE NOT NULL,
+	id_cartao SERIAL NOT NULL);
+
+	select * from cartao_aluno;
+
+	insert into cartao_aluno(nome,matricula,cpf) values
+	('Aluisio Veloso Silva Carvalho','2412130165','12345678912'),
+	('Miguel Ferreira Pedroso','2412130072','23456789123'),
+	('Paula Ribeiro Moreira de Souza','2412130037','34567891234'),
+	('Joao Enrique Oliveira Souza','2412130084','45678912345'),
+	('Pedro Arthur Silva Marques','2422130049','56789123456')
+
+
+	create table sala_estudo (
+	id_sala SERIAL PRIMARY KEY,
+	hora_entrada time,
+	hora_saida time,
+	matricula VARCHAR (10) REFERENCES cartao_aluno(matricula));
+
+	create table emprestimo (
+	id_livro SERIAL PRIMARY KEY,
+	data_emprestimo DATE,
+	data_devoluçao DATE,
+	matricula VARCHAR (10) REFERENCES cartao_aluno(matricula));
+
+	create table catraca(
+	id_catraca SERIAL PRIMARY KEY,
+	hora_entrada TIME,
+	hora_saida TIME,
+	matricula VARCHAR (10) REFERENCES cartao_aluno(matricula));
+
+	create table desconto (
+	id_desconto SERIAL PRIMARY KEY,
+	percentual_desconto DECIMAL (4, 2) NOT NULL,
+	matricula VARCHAR (10) REFERENCES cartao_aluno(matricula));
+
+	create table estabelecimento (
+	hora_abertura TIME NOT NULL,
+	hora_fechamento TIME NOT NULL,
+	preço INT NOT NULL,
+	tipo INT PRIMARY KEY,
+	id_desconto SERIAL REFERENCES desconto(id_desconto));
+
+	create table cantina (
+	comida VARCHAR(50),
+	tipo INT REFERENCES estabelecimento(tipo),
+	id_cantina INT PRIMARY KEY);
+	
+	create table estacionamento (
+	quantidade_vagas INT NOT NULL,
+	hora_entrada TIME,
+	hora_saida TIME,
+	tipo INT REFERENCES estabelecimento(tipo));
+
+
+
+
+
+
+
+
+
+    drop table aluno;
+	drop table sala_estudo;
+	drop table emprestimo;
+	drop table catraca;
+	drop table desconto;
+	drop table estacionamento;
+	drop table cartao;
+
+
+
+
+
+	
+	
