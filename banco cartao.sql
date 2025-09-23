19bc0467bc5b0a7cf006b59fe11c863af114692c
alu180 <162990810+alu180@users.noreply.github.com>
2025-09-23 13:58:12 -0300

    Add files via upload
    populado sala de estudo

--- banco cartao.sql
+++ banco cartao.sql
@@ -15,13 +15,14 @@ CREATE SCHEMA IF NOT EXISTS flexcard
 
 	select * from cartao_aluno;
 
-	insert into cartao_aluno(nome,matricula,cpf,data_emissao,data_validade) values
+	insert into cartao_aluno(nome,matricula,cpf,data_emissao,data_validade) VALUES
 	('Aluisio Veloso Silva Carvalho','2412130165','12345678912','01/01/20','02/02/20'),
 	('Miguel Ferreira Pedroso','2412130072','23456789123','02/02/21','03/03/21'),
 	('Paula Ribeiro Moreira de Souza','2412130037','34567891234','03/03/22','04/04/22'),
 	('Joao Enrique Oliveira Souza','2412130084','45678912345','04/04/23','05/05/23'),
 	('Pedro Arthur Silva Marques','2422130049','56789123456','05/05/24','06/06/24')
 
+-- add data em sala de estudos?
 
 	create table sala_estudo (
 	id_sala SERIAL PRIMARY KEY,
@@ -29,11 +30,24 @@ CREATE SCHEMA IF NOT EXISTS flexcard
 	hora_saida time,
 	matricula VARCHAR (10) REFERENCES cartao_aluno(matricula));
 
+	select * from sala_estudo;
+
+	insert into sala_estudo(hora_entrada,hora_saida,matricula) VALUES
+	('01:00','02:00','2412130165'),
+	('20:00','21:00','2412130072'),
+	('04:00','05:00','2412130037'),
+	('15:00','16:00','2412130084'),
+	('19:00','22:00','2422130049')
+
 	create table emprestimo (
 	id_livro SERIAL PRIMARY KEY,
 	data_emprestimo DATE,
 	data_devoluçao DATE,
 	matricula VARCHAR (10) REFERENCES cartao_aluno(matricula));
+
+	select * from emprestimo;
+
+	insert into
 
 	create table catraca(
 	id_catraca SERIAL PRIMARY KEY,
